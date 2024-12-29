309f2d71384d23cfc2a284d17a8b7261e346deb1
0sureshyadav0 <suresh.y.np@gmail.com>
2024-12-29 07:04:42 -0500

    initial commit

---
+++ assets/assets/readme/README.md
@@ -0,0 +1,206 @@
+# 📱 GlassNavBar
+
+---
+
+A customizable navigation bar for Flutter apps with a frosted glass effect. The navigation bar allows you to set custom icons, labels, and colors for both selected and unselected states. It uses the **BackdropFilter** widget to create the glass effect with a blur.
+
+---
+
+## ✨ Features
+
+- **Frosted Glass Effect**: Achieved using **BackdropFilter** and **Container** for a smooth, modern UI.
+- **Customizable Colors**: Set your preferred icon and text colors for both active and inactive states.
+- **Flexible Item Configuration**: Accepts a list of **NavBarItem** objects to define navigation items.
+
+---
+
+## 🚀 Installation
+
+### 1. Add the dependency to your **pubspec.yaml**:
+
+```yaml
+dependencies:
+  glass_nav_bar: ^0.0.4                                                       .
+```
+
+### 2.Install the package:
+
+```dart
+flutter pub get                                                        .
+```
+
+---
+
+## 💡 Usage
+
+To use the GlassNavBar in your app, follow these steps:
+
+### 1. Define the NavBarItem objects
+
+- Create a list of NavBarItem objects. Each item will have a label and an icon.
+
+```dart
+final List<NavBarItem> items = [
+  NavBarItem(label: 'Home', icon: Icons.home),
+  NavBarItem(label: 'Search', icon: Icons.search),
+  NavBarItem(label: 'Profile', icon: Icons.account_circle),
+];
+```
+
+### 2. Add GlassNavBar to your Widget Tree
+
+- Use the GlassNavBar widget inside your screen and pass the necessary parameters.
+
+```dart
+GlassNavBar(
+  selectedIndex: 0, // The index of the selected item
+  onItemTapped: (index) {
+    // Handle the tap event here
+  },
+  textColor: Colors.white, // Color for unselected text
+  iconColor: Colors.white60, // Color for unselected icons
+  activeTextColor: Colors.blue, // Color for selected text
+  activeIconColor: Colors.blue, // Color for selected icons
+  items: items, // The list of items for the nav bar
+)
+```
+
+---
+
+## ✏️ Parameters:
+
+- **selectedIndex**: The index of the currently selected navigation item.
+- **onItemTapped**: A callback function that gets called when an item is tapped, passing the index of the tapped item.
+- **textColor**: Color for the text of unselected items.
+- **activeTextColor**: Color for the text of the selected item.
+- **iconColor**: Color for the icon of unselected items.
+- **activeIconColor**: Color for the icon of the selected item.
+- **items**: A list of NavBarItem objects that define the labels and icons for each navigation item.
+
+---
+
+## </> Example
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:glass_nav_bar/glass_nav_bar.dart';
+import 'package:glass_nav_bar/nav_bar_item.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Glass Nav Bar',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Glass Nav Bar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: GlassNavBar(
+          selectedIndex: _selectedIndex,
+          onItemTapped: (newIndex) {
+            setState(() {
+              _selectedIndex = newIndex;
+            });
+          },
+          textColor: Colors.black,
+          iconColor: Colors.black54,
+          activeTextColor: Colors.pink,
+          activeIconColor: Colors.pink,
+          items: [
+            NavBarItem(icon: Icons.home, label: "Home"),
+            NavBarItem(icon: Icons.notifications, label: "Notifications"),
+            NavBarItem(icon: Icons.settings, label: "Settings"),
+            NavBarItem(icon: Icons.person, label: "Profile"),
+          ]),
+    );
+  }
+
+  int _selectedIndex = 0;
+}
+
+```
+
+---
+
+## 🔧 Customization
+
+- The GlassNavBar widget allows for customization of the appearance of the nav bar by adjusting the colors of icons and text based on the selected index.
+- You can also adjust the shape of the nav bar by modifying the borderRadius in the ClipRRect.
+
+---
+
+## 🧑‍💻 Developer
+
+- 🙋‍♂️ **Suresh Yadav**
+- 🌐 [sureshyadav.info.np](http://sureshyadav.info.np)
+- 📧 [Contact](mailto:hi@sureshyadav.info.np)
+
+---
+
+## 🌏 License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/0sureshyadav0/glass_nav_bar/blob/master/LICENSE "MIT License") file for details.
+
+---
+
+## 🤝 Contributions
+
+If you'd like to contribute to this project, feel free to fork the repository, create a new branch, and submit a pull request [here](https://github.com/0sureshyadav0/glass_nav_bar "GitHub Repo"). Any improvements or fixes are welcome!
+
+---
---
+++ flutter_service_worker.js
@@ -0,0 +1,228 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"index.html": "e8d554dfae81448fb0b82cf66e38f19f",
+"/": "e8d554dfae81448fb0b82cf66e38f19f",
+"flutter_bootstrap.js": "e3eb2401ac16aeae8cc29b8ce0780ecc",
+"manifest.json": "8d49e1b8bd99deef7551d67caed94929",
+"version.json": "d92d97e0eb76dcea4dd97dc220e8ff3d",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"assets/AssetManifest.bin": "44123f1b904ff1fa9b4fe48cd6db412c",
+"assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.html": "16911fcc170c8af1c5457940bd0bf055",
+"assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.css": "5a8d0222407e388155d7d1395a75d5b9",
+"assets/packages/flutter_inappwebview_web/assets/web/web_support.js": "509ae636cfdd93e49b5a6eaf0f06d79f",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "a2eb084b706ab40c90610942d98886ec",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bf21cd8fd775a3c59fd53afdee39e0e6",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3bb40a664d30151784350a69dbd31308",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/AssetManifest.bin.json": "e7cf40e21135b0dccfcd5cba2424950f",
+"assets/NOTICES": "1cea601fd38612e9f3647bdc132771ce",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
+"assets/assets/lottie/whatsapp.json": "3a71f8fe1b145ec7573e98762a6c8986",
+"assets/assets/lottie/resume.json": "4971cfde8ab408f25d7203731ced3dc7",
+"assets/assets/lottie/download.json": "6d3b3c4e9de90170fc8b8b39a7ae7b0d",
+"assets/assets/lottie/businessman.json": "09a5afb1bafde5a73536fef9138a5aed",
+"assets/assets/lottie/hi.json": "4a5b187d7a72b4bf2fae1cf3a828fca9",
+"assets/assets/lottie/website.json": "05b72e11ed946cda3b3e2a2fa8aaf234",
+"assets/assets/lottie/github.json": "ca02963e252e3619fa45582c7324929c",
+"assets/assets/lottie/package.json": "528f614f34eb94bec69869b9db180faa",
+"assets/assets/lottie/how.json": "a14707d963808b700cdae3cd974fc3be",
+"assets/assets/lottie/Parrot(1).json": "4747af15979d890aa9a19cd5dd011d46",
+"assets/assets/lottie/website1.json": "9db6523f3a78920c3d84ba979804e85b",
+"assets/assets/lottie/ani1.json": "e227306db570ab45be9b3991ffc8f462",
+"assets/assets/images/appicon.png": "cb28dc14b4e9c6eb37e8ac6418e74db6",
+"assets/assets/images/login_background.jpeg": "7781c439d22e0a42baea383494b1767d",
+"assets/assets/images/backgroundimg.jpeg": "4429134bcc53bc8d7ac788e0ba3de9d0",
+"assets/assets/images/google_icon.png": "7d2d7ef5aaa41fb4a82e81a8b16b1a3a",
+"assets/assets/readme/README.md": "364919b39a7775be886cf8bee8881aa9",
+"assets/AssetManifest.json": "30abdbb52b84a74f0284077da5c480a9",
+"assets/fonts/MaterialIcons-Regular.otf": "74b99f0a1e9caadcad9381d412a30de0",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"main.dart.js": "cde276fc95925d6b0cb6ee7a1b1afff2",
+"flutter.js": "4b2350e14c6650ba82871f60906437ea",
+"canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
+"canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
+"canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
+"canvaskit/chromium/canvaskit.wasm": "ea5ab288728f7200f398f60089048b48",
+"canvaskit/chromium/canvaskit.js": "b7ba6d908089f706772b2007c37e6da4",
+"canvaskit/chromium/canvaskit.js.symbols": "e115ddcfad5f5b98a90e389433606502",
+"canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
+"canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
+"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
