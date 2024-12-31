f438dd35e383ed0a281f9227806dae51011cf49b
0sureshyadav0 <suresh.y.np@gmail.com>
2024-12-31 01:51:56 -0500

    no comment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "675532026"
+    serviceWorkerVersion: "4250450080"
   }
 });
