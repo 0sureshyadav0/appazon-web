c2e73c60cdb96009028d0d300d4375279ccad336
0sureshyadav0 <suresh.y.np@gmail.com>
2024-12-31 00:46:12 -0500

    another

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2191942393"
+    serviceWorkerVersion: "675532026"
   }
 });
